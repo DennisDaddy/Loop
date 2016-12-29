502001b182113cf22079c466f3d09c210c376451
DennisDaddy <softteez95@gmail.com>
2016-12-29 07:51:56 +0300

    add index to true

--- db/migrate/20161229044652_create_notifications.rb
+++ db/migrate/20161229044652_create_notifications.rb
@@ -1,9 +1,9 @@
 class CreateNotifications < ActiveRecord::Migration[5.0]
   def change
     create_table :notifications do |t|
-      t.references :notifiable, polymorphic: true
+      t.references :notifiable, polymorphic: true, index: true
 
-      t.timestamps
+      t.timestamps null:false
     end
   end
 end
