becae187e8d6b738f94b28b0df8c8d531ffbd3f5
DennisDaddy <softteez95@gmail.com>
2017-01-24 12:11:33 +0300

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124090501) do
+ActiveRecord::Schema.define(version: 20170124091029) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.boolean  "status"
+    t.integer  "post_id"
+    t.integer  "visitor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["visitor_id"], name: "index_comments_on_visitor_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
