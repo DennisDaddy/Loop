f7399ea1068a8b71226cdcde7ef816420a5e7484
DennisDaddy <softteez95@gmail.com>
2017-01-25 08:30:24 +0300

    add validations

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,4 +1,8 @@
 class Moderator < ApplicationRecord
 	has_secure_password
 	has_many :posts
+
+	validates :fullname, presence: true
+	validates :username, presence: true
+	validates :password, presence: true
 end
