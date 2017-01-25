a832f4dcec368d7bf19b6b4d69454990a4960dbb
DennisDaddy <softteez95@gmail.com>
2017-01-25 09:33:19 +0300

    generate controller admin/session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    get 'sessions/new'
+  end
+
+  namespace :admin do
+    get 'sessions/create'
+  end
+
+  namespace :admin do
+    get 'sessions/destroy'
+  end
+
+  namespace :admin do
   resources :moderators, only: [:index, :edit, :update]
 end
 end
