4f8414ccc4c15da1e81fcd9c6e2f0ec837eda33d
DennisDaddy <softteez95@gmail.com>
2017-01-25 09:56:23 +0300

    create authorize and current_moderator action

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,14 @@
-class Admin::ApplicationController
-	
+class Admin::ApplicationController < ApplicationController
+	def current_moderator
+		false		
+	end
+
+	def authorize
+		unless current_moderator
+			redirect_to '/login', alert: 'please login to view admin pages'
+			
+		end
+		
+	end
+
 end
