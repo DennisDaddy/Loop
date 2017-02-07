52c50d339b06e5f45d3855c97100f48a3ec1c06b
DennisDaddy <softteez95@gmail.com>
2017-02-07 14:56:36 +0300

    complete search

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,11 @@
 class Admin::PostsController < Admin::ApplicationController
-  def index
+   def index
+
+     if params[:search].present?
+    @posts = Post.matching_title_or_content(params[:search]).page params[:page]
+    else
     @posts = Post.all.order(id: :desc).page params[:page]
+  end
   end
 
   def new
