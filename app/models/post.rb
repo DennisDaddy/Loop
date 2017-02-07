aed647fad2d824f3fdc3d1bae6d42de7ab5bc0ca
DennisDaddy <softteez95@gmail.com>
2017-02-07 17:20:42 +0300

    add post dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many  :tags, through: :post_tags
   belongs_to :moderator
